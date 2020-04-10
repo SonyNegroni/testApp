3d69b1d366ce91b2f01d42d322cd7d5ea6b89bb7
Taras <55309663+SonyNegroni@users.noreply.github.com>
2020-04-10 12:33:40 +0300

    Revert "Log in component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-/* Routing */
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
-/* Angular Material */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material.module';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
-/* FormsModule */
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { AuthenticationModule } from './authentication/authentication.module';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    
-    
- ],
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    AngularMaterialModule,
-    ReactiveFormsModule,
-    FormsModule,
-    FlexLayoutModule,
-    AuthenticationModule
-    
-  ],
-  exports: [
-    AuthenticationModule
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  bootstrap: [AppComponent]
 })
-
 export class AppModule { }
